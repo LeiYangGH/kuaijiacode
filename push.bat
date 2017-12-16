c80452889e44e6cf57f9693f3089041894ea5f92
yang.lei <yang.lei@nuance.com>
2017-12-16 09:34:53 +0800

    can view food1 in home dev view

--- push.bat
+++ push.bat
@@ -4,7 +4,7 @@ if not defined msg (echo "msg" not defined ) else (
 git add .
 git commit -m "%msg%"
 echo 'pushing...'
-git push origin
+git push --set-upstream origin kuaijia
 echo 'end'
 )
 pause
